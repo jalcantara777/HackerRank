a4c5d47640a40f584618d40aced08a7cb400fd8b
José Alcántara <jalcantara777@yahoo.com>
2024-08-05 10:41:30 -0500

    Upload Codes 2nd practice

---
+++ Basic/Permutations.py
@@ -0,0 +1,11 @@
+from itertools import permutations
+cad=input().upper()
+k=int(input())
+lstperm= list(permutations(cad, k))
+lst2ret=[]
+for i in range(len(lstperm)):
+    cadaux=''.join(str(x) for x in lstperm[i])
+    lst2ret.append(cadaux)
+lst2ret.sort()
+for i in range(len(lst2ret)):
+    print(lst2ret[i])
