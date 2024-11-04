7849349570ca5880efb4839e5c65becb7d498bf0
José Alcántara <141802328+jalcantara777@users.noreply.github.com>
2024-11-04 16:57:34 -0500

    Time Delta proposal - update

--- Medium/Time Delta - proposal.py
+++ Medium/Time Delta - proposal.py
@@ -1,3 +1,12 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the time_delta function below.
+def time_delta(t1, t2):
+
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
     t = int(input())
