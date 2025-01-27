afd67390aea715370033d2e3b73cb0f63e26b684
José Alcántara <141802328+jalcantara777@users.noreply.github.com>
2025-01-27 11:06:39 -0500

    Re_findall-Re_finditer solution all cases

--- Basic/Re_findall-Re_finditer.py
+++ Basic/Re_findall-Re_finditer.py
@@ -1,14 +1,13 @@
 import re
 
-VOCALES = "aeiou"
-CONSONANTES = "bcdfghjklmnpqrstvwxyz"
-pattern = rf"[{VOCALES}]([{CONSONANTES}])\1+[{VOCALES}]"
-matches = re.finditer(pattern, input())
+VOCALES = "aeiouAEIOU"
+CONSONANTES = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+pattern = rf"(?<=[{CONSONANTES}])([{VOCALES}]{{2,}})(?=[{CONSONANTES}])"
 
-# Extraemos las subcadenas completas que contienen las consonantes repetidas
-result = [match.group(0)[1:-1] for match in matches]
+result = re.findall(pattern, input())
 
 if len(result) > 0:
-    for i in range(len(result)): print(result[i])
+    for i in range(len(result)): 
+        print(result[i])
 else:
     print(-1)
